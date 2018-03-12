a143d2f5419308ec79a6f33c1e4ce940eb9eb6ed
Calvin Grewal <calvingrewal28@gmail.com>
2018-03-11 23:21:32 -0700

    manually transpiled to es5

--- src/number.js
+++ src/number.js
@@ -1,23 +1,33 @@
-/* eslint no-console: 0 */
-export function oldNumber(a, b) {
-  return a = +a, b -= a, function(t) {
-    return a + b * t;
-  };
-}
+"use strict";
 
-export default function(a, b, strictClockwise = false, angle = 0) {
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.oldNumber = oldNumber;
+
+exports.default = function (a, b) {
+  var strictClockwise = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : false;
+  var angle = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 0;
+
   var start = +a;
   var end = b;
   if (strictClockwise) {
-    if (end < angle && angle < start) { //end < angle < start
-      end += 360
+    if (end < angle && angle < start) {
+      //end < angle < start
+      end += 360;
     }
     if (start < angle && angle < end) {
-      start += 360
+      start += 360;
     }
   }
-  return function(t) {
-    return start + (end - start) *t
+  return function (t) {
+    return start + (end - start) * t;
   };
+};
 
+/* eslint no-console: 0 */
+function oldNumber(a, b) {
+  return a = +a, b -= a, function (t) {
+    return a + b * t;
+  };
 }
--- src/transform/index.js
+++ src/transform/index.js
@@ -1,8 +1,23 @@
-/* eslint no-console: 0 */
-import number from "../number";
-import {parseCss, parseSvg} from "./parse";
+e strict";
 
-function interpolateTransform(parse, pxComma, pxParen, degParen, strictClockwise = false, angle=0) {
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.interpolateTransformSvg = exports.interpolateTransformCss = undefined;
+
+var _number = require("../number");
+
+var _number2 = _interopRequireDefault(_number);
+
+var _parse = require("./parse");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/* eslint no-console: 0 */
+function interpolateTransform(parse, pxComma, pxParen, degParen) {
+  var strictClockwise = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : false;
+  var angle = arguments.length > 5 && arguments[5] !== undefined ? arguments[5] : 0;
+
 
   function pop(s) {
     return s.length ? s.pop() + " " : "";
@@ -11,7 +26,7 @@ function interpolateTransform(parse, pxComma, pxParen, degParen, strictClockwise
   function translate(xa, ya, xb, yb, s, q) {
     if (xa !== xb || ya !== yb) {
       var i = s.push("translate(", null, pxComma, null, pxParen);
-      q.push({i: i - 4, x: number(xa, xb)}, {i: i - 2, x: number(ya, yb)});
+      q.push({ i: i - 4, x: (0, _number2.default)(xa, xb) }, { i: i - 2, x: (0, _number2.default)(ya, yb) });
     } else if (xb || yb) {
       s.push("translate(" + xb + pxComma + yb + pxParen);
     }
@@ -28,7 +43,7 @@ function interpolateTransform(parse, pxComma, pxParen, degParen, strictClockwise
 
       q.push({
         i: s.push(pop(s) + "rotate(", null, degParen) - 2,
-        x: number(a, b, strictClockwise, angle)
+        x: (0, _number2.default)(a, b, strictClockwise, angle)
       });
     } else if (b) {
       s.push(pop(s) + "rotate(" + b + degParen);
@@ -37,7 +52,7 @@ function interpolateTransform(parse, pxComma, pxParen, degParen, strictClockwise
 
   function skewX(a, b, s, q) {
     if (a !== b) {
-      q.push({i: s.push(pop(s) + "skewX(", null, degParen) - 2, x: number(a, b)});
+      q.push({ i: s.push(pop(s) + "skewX(", null, degParen) - 2, x: (0, _number2.default)(a, b) });
     } else if (b) {
       s.push(pop(s) + "skewX(" + b + degParen);
     }
@@ -46,28 +61,32 @@ function interpolateTransform(parse, pxComma, pxParen, degParen, strictClockwise
   function scale(xa, ya, xb, yb, s, q) {
     if (xa !== xb || ya !== yb) {
       var i = s.push(pop(s) + "scale(", null, ",", null, ")");
-      q.push({i: i - 4, x: number(xa, xb)}, {i: i - 2, x: number(ya, yb)});
+      q.push({ i: i - 4, x: (0, _number2.default)(xa, xb) }, { i: i - 2, x: (0, _number2.default)(ya, yb) });
     } else if (xb !== 1 || yb !== 1) {
       s.push(pop(s) + "scale(" + xb + "," + yb + ")");
     }
   }
 
-  return function(a, b) {
-    var s = [], // string constants and placeholders
-        q = []; // number interpolators
+  return function (a, b) {
+    var s = [],
+        // string constants and placeholders
+    q = []; // number interpolators
     a = parse(a), b = parse(b);
     translate(a.translateX, a.translateY, b.translateX, b.translateY, s, q);
     rotate(a.rotate, b.rotate, s, q);
     skewX(a.skewX, b.skewX, s, q);
     scale(a.scaleX, a.scaleY, b.scaleX, b.scaleY, s, q);
     a = b = null; // gc
-    return function(t) {
-      var i = -1, n = q.length, o;
-      while (++i < n) s[(o = q[i]).i] = o.x(t);
-      return s.join("");
+    return function (t) {
+      var i = -1,
+          n = q.length,
+          o;
+      while (++i < n) {
+        s[(o = q[i]).i] = o.x(t);
+      }return s.join("");
     };
   };
 }
 
-export var interpolateTransformCss = interpolateTransform(parseCss, "px, ", "px)", "deg)");
-export var interpolateTransformSvg = interpolateTransform(parseSvg, ", ", ")", ")", true, -90);
+var interpolateTransformCss = exports.interpolateTransformCss = interpolateTransform(_parse.parseCss, "px, ", "px)", "deg)");
+var interpolateTransformSvg = exports.interpolateTransformSvg = interpolateTransform(_parse.parseSvg, ", ", ")", ")", true, -90);
